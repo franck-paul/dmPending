4d789a955ff7a9c15fa62122b346f7a0a69d9d28
franck <carnet.franck.paul@gmail.com>
2018-03-09 15:46:42 +0100

    Various code formatting

--- _install.php
+++ _install.php
@@ -1,40 +1,41 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of dmPending, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief dmPending, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_CONTEXT_ADMIN')) { return; }
+if (!defined('DC_CONTEXT_ADMIN')) {return;}
 
-$new_version = $core->plugins->moduleInfo('dmPending','version');
+$new_version = $core->plugins->moduleInfo('dmPending', 'version');
 $old_version = $core->getVersion('dmPending');
 
-if (version_compare($old_version,$new_version,'>=')) return;
+if (version_compare($old_version, $new_version, '>=')) {
+    return;
+}
 
 try
 {
-	$core->auth->user_prefs->addWorkspace('dmpending');
+    $core->auth->user_prefs->addWorkspace('dmpending');
 
-	// Default prefs for pending posts and comments
-	$core->auth->user_prefs->dmpending->put('pending_posts',false,'boolean','Display pending posts',false,true);
-	$core->auth->user_prefs->dmpending->put('pending_posts_nb',5,'integer','Number of pending posts displayed',false,true);
-	$core->auth->user_prefs->dmpending->put('pending_posts_large',true,'boolean','Large display',false,true);
-	$core->auth->user_prefs->dmpending->put('pending_comments',false,'boolean','Display pending comments',false,true);
-	$core->auth->user_prefs->dmpending->put('pending_comments_nb',5,'integer','Number of pending comments displayed',false,true);
-	$core->auth->user_prefs->dmpending->put('pending_comments_large',true,'boolean','Large display',false,true);
+    // Default prefs for pending posts and comments
+    $core->auth->user_prefs->dmpending->put('pending_posts', false, 'boolean', 'Display pending posts', false, true);
+    $core->auth->user_prefs->dmpending->put('pending_posts_nb', 5, 'integer', 'Number of pending posts displayed', false, true);
+    $core->auth->user_prefs->dmpending->put('pending_posts_large', true, 'boolean', 'Large display', false, true);
+    $core->auth->user_prefs->dmpending->put('pending_comments', false, 'boolean', 'Display pending comments', false, true);
+    $core->auth->user_prefs->dmpending->put('pending_comments_nb', 5, 'integer', 'Number of pending comments displayed', false, true);
+    $core->auth->user_prefs->dmpending->put('pending_comments_large', true, 'boolean', 'Large display', false, true);
 
-	$core->setVersion('dmPending',$new_version);
+    $core->setVersion('dmPending', $new_version);
 
-	return true;
-}
-catch (Exception $e)
-{
-	$core->error->add($e->getMessage());
+    return true;
+} catch (Exception $e) {
+    $core->error->add($e->getMessage());
 }
 return false;
