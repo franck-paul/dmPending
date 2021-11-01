f9cc0bb9e12e56f33ffe71dc9f860127b1c36bb4
franck <carnet.franck.paul@gmail.com>
2021-11-01 09:30:35 +0100

    Update default branch in _define.php for alternative repository

--- _define.php
+++ _define.php
@@ -29,6 +29,6 @@ $this->registerModule(
 
         'details'    => 'https://open-time.net/?q=dmPending',       // Details URL
         'support'    => 'https://github.com/franck-paul/dmPending', // Support URL
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/dmPending/main/dcstore.xml'
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/dmPending/master/dcstore.xml'
     ]
 );
