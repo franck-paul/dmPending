85a1b6e94c4a66830da19c1d7bc3cca60a240f6c
franck <carnet.franck.paul@gmail.com>
2024-01-07 07:57:03 +0100

    New release 4.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Pending Dashboard Module',
     'Display pending posts and comments on dashboard',
     'Franck Paul',
-    '4.1',
+    '4.2',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
